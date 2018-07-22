d521bb1578ad20f082ae9444c280328f5212369a
Unknown <eti.aurand@gmail.com>
2018-07-22 05:55:10 +0200

    Symfony 3 adjustments
    According to https://knpuniversity.com/screencast/symfony3-upgrade/new-dir-structure

--- web/app.php
+++ web/app.php
@@ -22,6 +22,7 @@ $loader->register(true);
  * @var \Composer\Autoload\ClassLoader $loader
  */
 $loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
